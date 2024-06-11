9f624fd8bfc2ed90f1c36cfea21c69f34b878dba
realdtn <doantrongnguyen170208@gmail.com>
2024-06-11 11:43:21 +0700

    Update sdcardfs.h

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -46,6 +46,7 @@
 #include <linux/string.h>
 #include <linux/list.h>
 #include "multiuser.h"
+#include <linux/uidgid.h>
 
 /* the file system name */
 #define SDCARDFS_NAME "sdcardfs"
@@ -553,8 +554,8 @@ static inline int prepare_dir(const char *path_s, uid_t uid, gid_t gid, mode_t m
 		goto out_dput;
 	}
 
-	attrs.ia_uid = (__force kuid_t)uid;
-	attrs.ia_gid = (__force kgid_t)gid;
+	attrs.ia_uid = __kuid_val(uid);
+	attrs.ia_gid = __kgid_val(gid);
 	attrs.ia_valid = ATTR_UID | ATTR_GID;
 	mutex_lock(&dent->d_inode->i_mutex);
 	notify_change2(parent.mnt, dent, &attrs);
@@ -624,8 +625,9 @@ static inline void sdcardfs_copy_and_fix_attrs(struct inode *dest, const struct 
 
 	dest->i_mode = (src->i_mode  & S_IFMT) | S_IRWXU | S_IRWXG |
 			S_IROTH | S_IXOTH; /* 0775 */
-	dest->i_uid = (__force kuid_t)SDCARDFS_I(dest)->data->d_uid;
-	dest->i_gid = (__force kgid_t)AID_SDCARD_RW;
+	dest->i_uid = __kuid_val(SDCARDFS_I(dest)->data->d_uid);
+	dest->i_gid = __kgid_val(AID_SDCARD_RW);
+	dest->i_rdev = src->i_rdev;
 	dest->i_atime = src->i_atime;
 	dest->i_mtime = src->i_mtime;
 	dest->i_ctime = src->i_ctime;
