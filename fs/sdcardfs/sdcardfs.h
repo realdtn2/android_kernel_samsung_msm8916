faae0b59c334d4ffa7bdfa5ca1b9dea0d8fcc8d1
realdtn <doantrongnguyen170208@gmail.com>
2024-06-11 11:24:02 +0700

    Update sdcardfs.h
    fix test

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -73,6 +73,10 @@
 
 #define AID_PACKAGE_INFO  1027
 
+#define TO_KUID(uid) ((kuid_t){ .val = uid })
+#define TO_KGID(gid) ((kgid_t){ .val = gid })
+
+
 
 /*
  * Permissions are handled by our permission function.
@@ -552,8 +556,8 @@ static inline int prepare_dir(const char *path_s, uid_t uid, gid_t gid, mode_t m
 		goto out_dput;
 	}
 
-	attrs.ia_uid = uid;
-	attrs.ia_gid = gid;
+	attrs.ia_uid = TO_KUID(uid);
+	attrs.ia_gid = TO_KGID(gid);
 	attrs.ia_valid = ATTR_UID | ATTR_GID;
 	mutex_lock(&dent->d_inode->i_mutex);
 	notify_change2(parent.mnt, dent, &attrs);
@@ -623,8 +627,8 @@ static inline void sdcardfs_copy_and_fix_attrs(struct inode *dest, const struct 
 
 	dest->i_mode = (src->i_mode  & S_IFMT) | S_IRWXU | S_IRWXG |
 			S_IROTH | S_IXOTH; /* 0775 */
-	dest->i_uid = SDCARDFS_I(dest)->data->d_uid;
-	dest->i_gid = AID_SDCARD_RW;
+	dest->i_uid = TO_KUID(SDCARDFS_I(dest)->data->d_uid);
+	dest->i_gid = TO_KGID(AID_SDCARD_RW);
 	dest->i_rdev = src->i_rdev;
 	dest->i_atime = src->i_atime;
 	dest->i_mtime = src->i_mtime;
